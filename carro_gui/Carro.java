fc6d97b89f161f5172543ad7944ce93c1e6e0c41
Blackhoodgx <65426541+Blackhoodgx@users.noreply.github.com>
2020-05-18 23:58:54 +0000

    Add files via upload
    usei o java application sample from para criar a janela

---
+++ carro_gui/Carro.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carro_gui;
+
+/**
+ *
+ * @author joao brilhante
+ */
+public class Carro {
+    boolean luz = false;
+    String matric;
+    double consum = 0;
+    String tipo;
+    int kms = 0;
+    double tanque = 0;
+    double ls = 0;
+    double dis =0;
+}
